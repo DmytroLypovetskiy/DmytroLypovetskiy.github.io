63d064a746f0c1fe00465e61d6753e14b4f10357
DmytroLypovetskiy <lipovetskiy@gmail.com>
2017-04-02 00:20:22 -0400

    Some updates

--- src/components/Intro.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const Intro = (props) => (
-    <div className="intro">
-        <h2>{props.data.title}</h2>
-        <h3>{props.data.role}</h3>
-        <p>{props.data.overview}</p>
-    </div>
-)
--- src/components/Section.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {WorkExperience} from '../components';
-
-export const Section = (props) => (
-    <section className="content">
-        <WorkExperience {...props} />
-    </section>
-)
